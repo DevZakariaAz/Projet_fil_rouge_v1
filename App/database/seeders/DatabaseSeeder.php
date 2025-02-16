9cc00c4c90eb7a2bd35ed4f212e6336d2d554302
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2025-02-16 17:49:16 +0100

    App in progresse

--- App/database/seeders/DatabaseSeeder.php
+++ App/database/seeders/DatabaseSeeder.php
@@ -1,21 +1,23 @@
 <?php
 
-// database/seeders/DatabaseSeeder.php
 namespace Database\Seeders;
 
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run()
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
     {
-        $this->call([
-            FormateurSeeder::class,
-            RHSeeder::class,
-            SeanceSeeder::class,
-            StagiaireSeeder::class,
-            RapportSeeder::class,
-            AbsenceSeeder::class,
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
