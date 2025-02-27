ddf86ea904fd14feb059c7a0962ecfc1cccb5c05
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2025-02-27 12:02:03 +0000

    App in progress

---
+++ App/resources/js/Layouts/AuthenticatedLayout2.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { Badge } from '@/Components/ui/badge'
+import { Link } from '@inertiajs/vue3';
+import { Button } from '@/Components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/Components/ui/card'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/Components/ui/dropdown-menu'
+import { Input } from '@/Components/ui/input'
+import { Sheet, SheetContent, SheetTrigger } from '@/Components/ui/sheet'
+import { Bell, CircleUser, Home, LineChart, Menu, Package, Package2, Search, ShoppingCart, Users } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div class="hidden border-r bg-muted/40 md:block">
+      <div class="flex h-full max-h-screen flex-col gap-2">
+        <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <a href="/" class="flex items-center gap-2 font-semibold">
+            <Package2 class="h-6 w-6" />
+            <span class="">Gestion des Absences</span>
+          </a>
+          <Button variant="outline" size="icon" class="ml-auto h-8 w-8">
+            <Bell class="h-4 w-4" />
+            <span class="sr-only">Toggle notifications</span>
+          </Button>
+        </div>
+        <div class="flex-1">
+          <nav class="grid items-start px-2 text-sm font-medium lg:px-4">
+            <a
+              href="/"
+              class="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+            >
+              <Home class="h-4 w-4" />
+              Dashboard
+            </a>
+            <a
+              href="#"
+              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <Users class="h-4 w-4" />
+              Saise l'Absence
+            </a>
+            <a
+              href="#"
+              class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <LineChart class="h-4 w-4" />
+              Analytics
+              <Badge class="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                6
+              </Badge>
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col">
+      <header class="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <div class="w-full flex-1">
+          <form>
+            <div class="relative">
+              <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Search ..."
+                class="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+              />
+            </div>
+          </form>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger as-child>
+            <Button variant="secondary" size="icon" class="rounded-full">
+              <CircleUser class="h-5 w-5" />
+              <span class="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Username : {{ $page.props.auth.user.name }}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem><Link :href="route('profile.edit')"> Profile </Link></DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem><Link :href="route('logout')" method="post">Logout</Link></DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </header>
+        <main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                <slot />
+        </main>
+    </div>
+  </div>
+</template>
